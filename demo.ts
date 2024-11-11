435976f17b67cf59740bc850cee349acd20a2251
antoniovocanec <cartier.tahor@gmail.com>
2024-11-11 20:03:50 +0100

    Initial setup

---
+++ demo.ts
@@ -0,0 +1,18 @@
+console.log("Hello World")
+
+function PozdraviCovjeka(name){
+  let greet;
+  if (name === "Antonio"){
+     greet = "Bok Antonio"
+  } else{
+     greet = "Bok"
+  }
+  console.log(greet);
+}
+PozdraviCovjeka("Antoni");
+
+for(let h=1; h<10; h++){
+    setTimeout(function(){
+        console.log(h);
+    },1000)
+}
